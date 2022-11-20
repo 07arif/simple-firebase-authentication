32987f94701ae4beac08c1b015013eaa6d68aaff
07arif <ai.arif0711@gmail.com>
2022-11-20 20:49:38 +0600

    user info, sign out, toggle sign in , sign out

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBKb_XmfcC0578_haocfz4L2wUOdfhqQ3U",
+  authDomain: "simple-firebase-authenti-45575.firebaseapp.com",
+  projectId: "simple-firebase-authenti-45575",
+  storageBucket: "simple-firebase-authenti-45575.appspot.com",
+  messagingSenderId: "609397173959",
+  appId: "1:609397173959:web:e45f6bcfc661e5dec658e2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
